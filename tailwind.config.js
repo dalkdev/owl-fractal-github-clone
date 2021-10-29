63378262ad23babaa0f25504cc28eb41d13e92cf
ohermanni <ohermanni@owl-digital.de>
2021-10-29 16:30:36 +0200

    Prefix für tailwind hinzugefügt und alle Handlebar-Files dementsprechend angepasst

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   purge: { layers: ['utilities'], content: ['./src/components/**/*.hbs'] },
+  prefix: 'nw-',
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
