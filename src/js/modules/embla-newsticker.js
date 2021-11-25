4068a98b214032053de5ed607027655002b12db3
ohermanni <ohermanni@owl-digital.de>
2021-11-25 12:02:32 +0100

    Zwischenstand Header

---
+++ src/js/modules/embla-newsticker.js
@@ -0,0 +1,20 @@
+import EmblaCarousel from 'embla-carousel';
+import { setupPrevNextBtns, disablePrevNextBtns } from './prevAndNextButtons';
+
+export function initNewsticker() {
+  const wrap = document.querySelector('.embla-newsticker');
+  const viewPort = wrap.querySelector('.embla-newsticker__viewport');
+  const prevBtn = wrap.querySelector('.embla-newsticker__button--prev');
+  const nextBtn = wrap.querySelector('.embla-newsticker__button--next');
+  const embla = EmblaCarousel(viewPort, {
+    align: 'start',
+    slidesToScroll: 1,
+    skipSnaps: false,
+  });
+  const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
+
+  setupPrevNextBtns(prevBtn, nextBtn, embla);
+
+  embla.on('select', disablePrevAndNextBtns);
+  embla.on('init', disablePrevAndNextBtns);
+}
