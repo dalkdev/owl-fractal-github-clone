aee36dd82811ebc2116f733e20c3becf447590c5
ohermanni <ohermanni@owl-digital.de>
2021-12-01 09:12:27 +0100

    Bugfixes Burgermenu

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -6,6 +6,17 @@ export function initBurgermenu() {
     document.getElementById('nw-visible-burger').classList.toggle('nw-hidden');
   }
 
+  // function toggleBurgerNavItem() {
+  //   const navItem = document.querySelector(this);
+  // }
+
+  document
+    .querySelector('a.nw-dropdown-indicator')
+    .addEventListener('click', function (e) {
+      const target = this.dataset.target;
+      document.getElementById(target).classList.toggle('open');
+    });
+
   document
     .getElementById('nw-burger-menu-switcher')
     .addEventListener('click', () => toggleBurgermenu());
