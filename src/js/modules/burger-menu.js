7a5a7d19e99b2396dbbfff13117fad6343e35912
ohermanni <ohermanni@owl-digital.de>
2021-12-01 15:00:37 +0100

    Bugfixes Burgermenu

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -1,21 +1,23 @@
 export function initBurgermenu() {
   function toggleBurgermenu() {
-    const burgermenu = document.getElementById('nw-sidebar-display-switch');
+    const burgermenu = document.getElementById('nw-sidebar-navigation');
     burgermenu.classList.toggle('nw-hidden');
     document.getElementById('nw-hidden-burger').classList.toggle('nw-hidden');
     document.getElementById('nw-visible-burger').classList.toggle('nw-hidden');
   }
 
-  // function toggleBurgerNavItem() {
-  //   const navItem = document.querySelector(this);
-  // }
+  const userSelection = document.getElementsByClassName('nw-dropdown-indicator');
 
-  document
-    .querySelector('a.nw-dropdown-indicator')
-    .addEventListener('click', function (e) {
+  for (let i = 0; i < userSelection.length; i++) {
+    userSelection[i].addEventListener('click', function () {
       const target = this.dataset.target;
       document.getElementById(target).classList.toggle('open');
     });
+  }
+
+  document
+    .getElementById('nw-burger-menu-hide')
+    .addEventListener('click', () => toggleBurgermenu());
 
   document
     .getElementById('nw-burger-menu-switcher')
