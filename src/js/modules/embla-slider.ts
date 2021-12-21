47c2b8083e09febbddae0c5fbcfcf602d2887700
CarmenBeshara <carmenbesharasaid2@gmail.com>
2021-12-21 16:42:58 +0100

    Slider optimization

--- src/js/modules/embla-slider.ts
+++ src/js/modules/embla-slider.ts
@@ -1,4 +1,5 @@
 import EmblaCarousel from 'embla-carousel';
+import { setupDotBtns, generateDotBtns, selectDotBtn } from './dotButtons';
 
 export function initSliders() {
   const sliderWrapperNodes = document.querySelectorAll('.slider');
@@ -12,31 +13,36 @@ function initSlider(wrapperNode: HTMLElement) {
   const counterCurrent = wrapperNode.querySelector('.slider--counter-current');
   const counterTotal = wrapperNode.querySelector('.slider--counter-total');
   const btnNext = wrapperNode.querySelector('.slider--button-next');
-  const btnPrev = wrapperNode.querySelector('.slider--button-prev');
+  /* const btnPrev = wrapperNode.querySelector('.slider--button-prev'); */
+  const dots = document.querySelector('.embla__dots');
 
   const mainCarouselWrap = wrapperNode.querySelector('.embla--main-carousel');
   const mainCarouselView = mainCarouselWrap?.querySelector('.embla__viewport');
   const mainCarousel = EmblaCarousel(mainCarouselView as HTMLElement, {
-    selectedClass: '',
-    loop: true,
-    skipSnaps: false,
+    dragFree: true,
+    containScroll: 'trimSnaps',
   });
+  const dotsArray = generateDotBtns(dots, mainCarousel);
+  const setSelectedDotBtn = selectDotBtn(dotsArray, mainCarousel);
+  setupDotBtns(dotsArray, mainCarousel);
+  mainCarousel.on('select', setSelectedDotBtn);
+  mainCarousel.on('init', setSelectedDotBtn);
 
-  const thumbCarouselWrap = wrapperNode.querySelector('.embla--thumb');
+  /* const thumbCarouselWrap = wrapperNode.querySelector('.embla--thumb');
   const thumbCarouselView =
     thumbCarouselWrap?.querySelector('.embla__viewport');
   const thumbCarousel = EmblaCarousel(thumbCarouselView as HTMLElement, {
     selectedClass: '',
     containScroll: 'keepSnaps',
     dragFree: true,
-  });
+  }); */
 
-  thumbCarousel.slideNodes().forEach((thumbNode, index) => {
+  /* thumbCarousel.slideNodes().forEach((thumbNode, index) => {
     thumbNode.addEventListener('click', () => {
       mainCarousel.scrollTo(index);
       thumbCarousel.scrollTo(index);
     });
-  });
+  }); */
 
   mainCarousel.on('init', () => {
     if (counterCurrent) {
@@ -49,14 +55,14 @@ function initSlider(wrapperNode: HTMLElement) {
     }
   });
 
-  mainCarousel.on('select', () => {
+  /* mainCarousel.on('select', () => {
     thumbCarousel.scrollTo(mainCarousel.selectedScrollSnap());
     if (counterCurrent) {
       counterCurrent.textContent = (
         mainCarousel.selectedScrollSnap() + 1
       ).toString();
     }
-  });
+  }); */
 
   if (btnNext) {
     btnNext.addEventListener('click', () => {
@@ -64,9 +70,9 @@ function initSlider(wrapperNode: HTMLElement) {
     });
   }
 
-  if (btnPrev) {
+  /* if (btnPrev) {
     btnPrev.addEventListener('click', () => {
       mainCarousel.scrollPrev();
     });
-  }
+  } */
 }
