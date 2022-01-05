608f66b0e37db53da74a3f7b2cea5f3946f9d0df
ohermanni <ohermanni@owl-digital.de>
2022-01-05 10:29:04 +0100

    Aktueller Stand des Headers mit Slide-In-Menü

--- src/js/modules/embla-navigation.js
+++ src/js/modules/embla-navigation.js
@@ -2,29 +2,32 @@ import EmblaCarousel from 'embla-carousel';
 import { setupPrevNextBtns, disablePrevNextBtns } from './prevAndNextButtons';
 
 export function initNavigation() {
-  setupNavigation('.embla-navigation');
-  setupNavigation('.embla-navigation-sticky');
-}
+  const setupEmblaCarousel = (emblaNode, options) => {
+    const viewPort = emblaNode.querySelector('.embla-navigation__viewport');
+    const prevBtn = emblaNode.querySelector('.embla-navigation__button--prev');
+    const nextBtn = emblaNode.querySelector('.embla-navigation__button--next');
+    const embla = EmblaCarousel(viewPort, options);
+    const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
 
-function setupNavigation(selector) {
-  const wrap = document.querySelector(selector);
-  const viewPort = wrap.querySelector(selector + '__viewport');
-  const prevBtn = wrap.querySelector(selector + '__button--prev');
-  const nextBtn = wrap.querySelector(selector + '__button--next');
-  const embla = EmblaCarousel(viewPort, {
+    if (prevBtn != null || nextBtn != null) {
+      setupPrevNextBtns(prevBtn, nextBtn, embla);
+      embla.on('select', disablePrevAndNextBtns);
+      embla.on('init', disablePrevAndNextBtns);
+      embla.on('scroll', disablePrevAndNextBtns);
+    }
+  };
+
+  const options = {
     loop: false,
     skipSnaps: true,
     align: 'start',
-  });
+  };
+  const emblaNodes = [].slice.call(
+    document.querySelectorAll('.embla-navigation')
+  );
+  const emblaCarousels = emblaNodes.map(emblaNode =>
+    setupEmblaCarousel(emblaNode, options)
+  );
 
-  const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
-
-  if (prevBtn != null || nextBtn != null)
-    setupPrevNextBtns(prevBtn, nextBtn, embla);
-
-  if (prevBtn != null || nextBtn != null) {
-    //  embla.on('select', disablePrevAndNextBtns);
-    embla.on('init', disablePrevAndNextBtns);
-    embla.on('scroll', disablePrevAndNextBtns);
-  }
+  return emblaCarousels;
 }
