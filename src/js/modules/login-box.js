9be59179f79d5ecd25d0fddbd2beb3cbead92be7
ohermanni <ohermanni@owl-digital.de>
2022-02-18 15:47:52 +0100

    TypeError im JS entfernt

--- src/js/modules/login-box.js
+++ src/js/modules/login-box.js
@@ -1,10 +1,11 @@
 export function initLoginBox() {
   function toggleLoginBox() {
     const loginBox = document.getElementById('nw-loginbox');
-    loginBox.classList.toggle('nw-hidden');
+    if (loginBox != null) loginBox.classList.toggle('nw-hidden');
   }
 
   const loginBoxButton = document.getElementById('nw-login-button');
 
-  loginBoxButton.addEventListener('click', toggleLoginBox);
+  if (loginBoxButton != null)
+    loginBoxButton.addEventListener('click', toggleLoginBox);
 }
