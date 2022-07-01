79684a84a16432316eaf3ac36729e3b5a143f9c9
ohermanni <ohermanni@owl-digital.de>
2022-07-01 09:37:11 +0200

    Accordion ausgelagert und angepasst.

--- src/js/modules/faqElement.js
+++ src/js/modules/faqElement.js
@@ -1,17 +1,12 @@
 export function initFAQ() {
-    const d = document;
-    const faqDropdown = d.getElementsByClassName(
-        'faq-opener'
-    );
+  const d = document;
+  const faqDropdown = d.getElementsByClassName('faq-opener');
 
-    for (let i = 0; i < faqDropdown.length; i++) {
-        faqDropdown[i].addEventListener('click', function (e) {
-            e.preventDefault();
-            this.nextElementSibling.classList.toggle("nw-hidden");
-
-
-
-        });
-    }
+  for (let i = 0; i < faqDropdown.length; i++) {
+    faqDropdown[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      this.classList.toggle('nw-show-accordion-item');
+      this.nextElementSibling.classList.toggle('nw-hidden');
+    });
+  }
 }
-
