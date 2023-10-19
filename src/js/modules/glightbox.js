ed6beb4d55b5d2c4adf951b82b6b23b7d597041f
ohermanni <ohermanni@owl-digital.de>
2023-10-19 12:52:55 +0200

    Improved lightbox generation to add ads.

--- src/js/modules/glightbox.js
+++ src/js/modules/glightbox.js
@@ -2,12 +2,48 @@ import GLightbox from '../../../node_modules/glightbox/src/js/glightbox';
 
 export function initGLightBox()
 {
+
     const galleryLightbox = GLightbox({
-        selector: '.nw-gallery-lightbox',
         touchNavigation: true,
         loop: true,
         moreText: 'mehr...'
     });
+
+
+    const lightboxes = document.querySelectorAll('.nw-gallery-lightbox');
+    const lightboxes_amount = lightboxes.length;
+    const adcontent = document.querySelector('#nw_ad');
+    let elements = [];
+
+    lightboxes.forEach((lightbox, index) => {
+        let el = {
+            'href': lightbox.href,
+            'type': 'image',
+            'caption': lightbox.title
+        }
+
+        elements.push(el);
+
+        lightbox.onclick = function(e)
+        {
+            e.preventDefault();
+            galleryLightbox.openAt(index);
+        };
+    });
+
+
+    galleryLightbox.setElements(elements);
+
+    for(let i = 0; i < galleryLightbox.elements.length; i++)
+    {
+        if (i % 5 == 0 && i > 0)
+        {
+            galleryLightbox.insertSlide({
+                content: '<p>Hier könnte Ihre Werbung stehen',
+                width: '90vw'
+            }, i);
+        }
+    }
 
     const articleImage = GLightbox({
         selector: '.nw-article-image',
