60bc1db484a3fc22c54a84d316a6951d1961842d
CarmenBeshara <carmenbesharasaid2@gmail.com>
2024-08-26 14:19:03 +0200

    rounded-buttons bauen, transition Effekte hinzufügen, Neue Änderungen in der Autorenseite

--- src/js/modules/dropdownMenu.js
+++ src/js/modules/dropdownMenu.js
@@ -5,6 +5,12 @@ export function initDropdownMenu() {
       const dropdownButton = dropdown.querySelector('.nw-dropdown-block');
       const dropdownSvg = dropdownButton.querySelector('svg');
 
+      const dropdownText = dropdownButton.querySelector('.nw-dropdown-text');
+      const dropdownMenu = dropdown.querySelector('ul');
+      const dropdownItems = dropdownMenu.querySelectorAll('li a');
+
+      const defaultOption = dropdownText.textContent;
+
       dropdownButton.addEventListener('click', () => {
         const dropdownMenu = dropdown.querySelector('ul');
         const isHidden = dropdownMenu.classList.contains('nw-hidden');
@@ -16,6 +22,16 @@ export function initDropdownMenu() {
           dropdownMenu.classList.add('nw-hidden');
           dropdownSvg.classList.remove('nw-rotate-180');
         }
+      });
+      dropdownItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+          event.preventDefault();
+          const selectedText = item.textContent;
+          dropdownText.textContent = selectedText === "Alle" ? defaultOption : selectedText;
+
+          dropdownMenu.classList.add('nw-hidden');
+          dropdownSvg.classList.remove('nw-rotate-180');
+        });
       });
     });
 }
